a7b7d6f1d1aed222df9bed5e122c13f00e5de78d
Anonymous <anon@example.com>
2026-02-20 09:35:08 +0100

    Added some mock data for visualization purposes

---
+++ frontend/src/data/fridgeItems.tsx
@@ -0,0 +1,27 @@
+//mock data for fridgeItems tab/screen
+export const mockFridgeItems = [
+    {
+        id: 1,
+        name: "Milk",
+        category: "Dairy",
+        quantity: 1,
+        price: 3.99,
+        expirationDate: "2024-07-01",
+    },
+    {
+        id: 2,
+        name: "Eggs",
+        category: "Dairy",
+        quantity: 12,
+        price: 2.99,
+        expirationDate: "2024-07-10",
+    },  
+    {
+        id: 3,
+        name: "Carrots",    
+        category: "Vegetables",
+        quantity: 5,
+        price: 0.99,
+        expirationDate: "2024-07-05",
+    },
+]
---
+++ frontend/src/data/groceryList.tsx
@@ -0,0 +1,21 @@
+//mock data for groceryList tab/screen
+export const mockGroceryItems = [
+    {
+        id: 1,
+        name: "Bread",
+        quantity: 1,
+        unit: "loaf",
+    },
+    {
+        id: 2,
+        name: "Butter",
+        quantity: 1,
+        unit: "stick",
+    },
+    {
+        id: 3,
+        name: "Apples",
+        quantity: 5,
+        unit: "pieces",
+    }
+]
