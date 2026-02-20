7fd582e98db546b5e530a6aee1a2a3d7e8e8357c
Anonymous <anon@example.com>
2026-02-20 09:36:24 +0100

    edited grocery tab to use reuseable components for showing items and buttons

--- frontend/app/(tabs)/groceryList.tsx
+++ frontend/app/(tabs)/groceryList.tsx
@@ -1,19 +1,23 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { fontSizes, fontFamily, fontWeights } from "@/themes/fonts";
+import { colors } from "@/themes/colors";
+import { mockGroceryItems } from "@/data/groceryList";
+import Custombutton from "@/components/buttons";
 
 export default function GroceryList() {
   return (
+    // grocery list screen - shows items to buy.
     <View style={styles.container}>
       <Text style={styles.title}>🛒 Grocery List</Text>
       <Text style={styles.subtitle}>Items to buy</Text>
-      <View style={styles.card}>
-        <Text>☐ Bread</Text>
-      </View>
-      <View style={styles.card}>
-        <Text>☐ Butter</Text>
-      </View>
-      <View style={styles.card}>
-        <Text>☐ Apples</Text>
-      </View>
+      {mockGroceryItems.map((item) => (
+        <View key={item.id} style={styles.card}>                
+        <Text style={styles.cardText}>{item.name}</Text>
+        <Text style={styles.cardText}>{item.quantity} {item.unit}</Text>
+        </View>
+      ))}
+      <Custombutton title="Add Item" onPress={() => console.log("Add item pressed")} />
+      <Custombutton title="remove item" onPress={() => console.log("remove item pressed")} />
     </View>
   );
 }
@@ -21,21 +25,24 @@ export default function GroceryList() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 35,
     padding: 20,
-    backgroundColor: "#f5f5f5",
+    backgroundColor: colors.background,
+    alignItems: "center",
   },
   title: {
-    fontSize: 28,
-    fontWeight: "bold",
+    paddingBottom: 20,
+    fontSize: fontSizes.header,
+    fontWeight: fontWeights.bold,
     marginBottom: 8,
   },
   subtitle: {
-    fontSize: 16,
-    color: "#666",
+    fontSize: fontSizes.subtitle,
+    color: colors.headerText,
     marginBottom: 20,
   },
   card: {
-    backgroundColor: "#fff",
+    backgroundColor: colors.primary,
     padding: 16,
     borderRadius: 8,
     marginBottom: 10,
@@ -43,5 +50,10 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 2,
+  },
+  cardText: {
+    color: colors.lightGray,
+    fontSize: fontSizes.body,
+    fontFamily: fontFamily.body,
   },
 });
