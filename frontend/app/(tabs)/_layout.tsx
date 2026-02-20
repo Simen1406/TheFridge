cefeb2e782c473bfa9aa26ca68e5d733558f3974
Anonymous <anon@example.com>
2026-02-20 12:48:43 +0100

    changed the names for menu (navigation elements

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -11,8 +11,8 @@ export default function TabsLayout() {
   return (
     <Tabs>
       <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="fridgeInventory" options={{ title: "Inventory" }} />
-      <Tabs.Screen name="gorceryList" options={{ title: "Grocery List" }} />
+      <Tabs.Screen name="Fridge" options={{ title: "Fridge" }} />
+      <Tabs.Screen name="Groceries" options={{ title: "Grocery" }} />
     </Tabs>
   );
 }
