99a289320849fbb30fa44adf70bfe37a9a76cded
Anonymous <anon@example.com>
2026-02-23 10:23:30 +0100

    created endpoint for adding new items in the UI, which then are sent to db

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from sqlmodel import Session, select
 
 from mockData.fridgeItems import insert_mock_data
 from db.db import init_db, get_session
-from models.models import FridgeItem
+from models.models import FridgeItem, AddFridgeItem
 
 
 #app entrypoint
@@ -35,3 +35,11 @@ def get_fridge_items(session: Session = Depends(get_session)):
     results = session.exec(select(FridgeItem)).all()
     print(type(results))
     return results
+
+@app.post("/AddFridgeItem")
+def add_fridge_item(item: AddFridgeItem, session: Session = Depends(get_session)):
+    new_item = FridgeItem.model_validate(item)
+    session.add(new_item)
+    session.commit()
+    session.refresh(new_item)
+    return new_item
