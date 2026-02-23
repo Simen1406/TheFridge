8197be4f3d6fd5b04412cfd3a62cde1d48085da3
Anonymous <anon@example.com>
2026-02-23 13:47:29 +0100

    Created food types for fridge and grocery items

---
+++ frontend/src/types/foodTypes.ts
@@ -0,0 +1,17 @@
+export type FridgeItem = {
+    id: number;
+    name: string;
+    category: string;
+    quantity: number;
+    unit: string;
+    expirationDate: string;
+};
+
+export type GroceryItem = {
+    id: number,
+    name: string,
+    category: string,
+    quantity: number,
+    unit: string,
+    expirationDate: string,
+};
